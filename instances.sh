fa4857b509cb00301f97d80e680bfb9aaf764682
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-10 21:54:32 +0530

    adding content service

--- instances.sh
+++ instances.sh
@@ -6,8 +6,8 @@ INSTANCES=("$@")
 ZONE_ID="Z00035852NN6D25PW7BUM" # replace with your ZONE ID
 DOMAIN_NAME="rajeshit.space" # replace with your domain
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0256c6a54027858fd --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
