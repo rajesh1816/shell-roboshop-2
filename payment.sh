b391ff27bb5b340cf978e34fd5837f83971b212e
rajesh <rajeshkaramsetty1999@gmail.com>
2025-11-10 20:54:19 +0530

    adding content service

--- payment.sh
+++ payment.sh
@@ -1,87 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-SCRIPT_START=$(date +%s)
+source ./common.sh
+appname=payment
+check_root
 
+app_setup
+python_setup
 
+systemd_setup
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo -e "$G You are running with root access $N" | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e " $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e " $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-
-dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-VALIDATE $? "installing python3"
-
-
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    echo -e " user is not existed ..$G so creating $N"
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    VALIDATE $? "creating roboshop systemuser"
-else
-    echo -e " roboshop user is already existed..$Y so skipping $N"
-fi
-
-
-
-mkdir -p /app 
-VALIDATE $? "creating app directory"
-
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading the payment zip file"
-
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the payment zip file"
-
-
-pip3 install -r requirements.txt &>>$LOG_FILE
-VALIDATE $? "installing dependencies"
-
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-VALIDATE $? "copying payment service file in /etc location for systemctl"
-
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "deamon-reload"
-
-systemctl enable payment &>>$LOG_FILE
-systemctl start payment
-VALIDATE $? "Enabling and starting the payment service"
-
-SCRIPT_END=$(date +%s)
-TOTAL_TIME=$(($SCRIPT_END-$SCRIPT_START))
-echo -e "Total time taken for installation: $G $TOTAL_TIME sec $N"
+print_time
 
 
 
